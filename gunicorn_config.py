f792dbdd668bc3739e00b8862a2d9d2042def428
Mohammed-Aswath <mohammed.aswath07@gmail.com>
2025-12-24 09:36:58 +0530

    Simplify Render deployment: use basic gunicorn command as per Render's Flask docs

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,17 +2,16 @@
 import os
 import multiprocessing
 
-# Get port from environment variable
-port = os.getenv('PORT', 5000)
+# Get port from environment variable (Render sets this)
+port = int(os.getenv('PORT', 8000))
 
-# Server socket - Render requires binding to 0.0.0.0:$PORT
+# Server socket
 bind = f"0.0.0.0:{port}"
 backlog = 2048
 
 # Worker processes
 workers = max(multiprocessing.cpu_count() - 1, 2)
 worker_class = "sync"
-worker_connections = 1000
 timeout = 120
 keepalive = 5
 
@@ -20,7 +19,6 @@ keepalive = 5
 accesslog = "-"
 errorlog = "-"
 loglevel = "info"
-access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s'
 
 # Process naming
 proc_name = "codeprac-backend"
@@ -28,6 +26,3 @@ proc_name = "codeprac-backend"
 # Server mechanics
 daemon = False
 pidfile = None
-umask = 0
-user = None
-group = None
